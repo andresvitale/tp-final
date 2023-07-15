5ab697e27632bca4f252e3e29156b0598364d908
andresdiridolfo <andresdiridolfo@gmail.com>
2023-07-15 16:20:11 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+    <footer class="mt-5">
+        <nav class="bg-dark">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center fuente">
+                    <p class="text-light">Aporte realizado por Andres Vitale
+                    <a class="text-ligth" style="color:white;" target="_blank" href="https://www.linkedin.com/in/andres-vitale-71a005151/"> 
+                    <script src="https://cdn.lordicon.com/lusqsztk.js"></script>
+                    LinkedIn</a>
+                </div>
+            </div>
+        </nav>
+    </footer>
+
+
+</div><!--cierro el container -->
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="envio.js"></script>
+</body>
+</html>
